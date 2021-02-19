6ca9a693da1b95d1f5949aecf160a03e394bd22e
Darian Wohlrabe <dcwohlrabe@gmail.com>
2021-02-19 16:38:49 -0600

    Home Page Contact Form

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList, ScrollView, StyleSheet } from 'react-native';
-import { Tile, Card, Button } from 'react-native-elements';
+import { View, Text, FlatList, ScrollView, StyleSheet, Modal } from 'react-native';
+import { Tile, Card, Button, Input } from 'react-native-elements';
 import { MODEL } from '../shared/model';
 
 class Home extends Component {
@@ -8,8 +8,22 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            model: MODEL
+            model: MODEL,
+            firstName: '',
+            lastName: '',
+            email: '',
+            phone:'',
+            feedback: '',
+            showModal: false
         };
+    }
+
+    handleModal() {
+        this.setState({showModal: !this.state.showModal})
+    }
+
+    submitForm() {
+        console.log(this.state.firstName, this.state.lastName, this.state.email, this.state.phone, this.state.feedback);
     }
 
     static navigationOptions = {
@@ -42,11 +56,51 @@ class Home extends Component {
                     keyExtractor={item => item.id.toString()}
                     renderItem={renderModel}
                 />
-                <Button 
+                 <Button 
                     title='Contact Us'
                     type='solid'
                     buttonStyle={styles.button}
+                    onPress={() => this.handleModal()}
                 />
+                <Modal visible={this.state.showModal}>
+                    <View style={styles.container}>
+                        <Text style={styles.pageHeader}>Contact Form</Text>
+                        <Input 
+                            placeholder='First Name'
+                            onChangeText={firstName => this.setState({firstName})}
+                            value={this.state.firstName}
+                        />
+                        <Input 
+                            placeholder='Last Name'
+                            onChangeText={lastName => this.setState({lastName})}
+                            value={this.state.lastName}
+                        />
+                        <Input 
+                            placeholder='Email'
+                            onChangeText={email => this.setState({email})}
+                            value={this.state.email}
+                        />
+                        <Input 
+                            placeholder='Phone Number'
+                            onChangeText={phone => this.setState({phone})}
+                            value={this.state.phone}
+                        />
+                        <Input 
+                            placeholder='Questions and Comments Here'
+                            onChangeText={feedback => this.setState({feedback})}
+                            value={this.state.feedback}
+                        />
+                        <Button 
+                            title='Submit'
+                            type='solid'
+                            buttonStyle={styles.button}
+                            onPress={() => {
+                                this.submitForm();
+                                this.handleModal();
+                            }}
+                        />
+                    </View>
+                </Modal>
             </ScrollView>
         );
     }
@@ -62,8 +116,12 @@ const styles = StyleSheet.create({
     },
     button: {
         margin: 40,
-        fontSize: 20
+        fontSize: 20,
+        backgroundColor: 'grey'
+    },
+    container: {
+        margin: 10
     }
-})
+});
 
 export default Home;
